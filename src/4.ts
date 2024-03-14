1ef4574cb0f52ada774f5078c0fd6cc827ba7bf3
MrRoik <138239220+MrRoik@users.noreply.github.com>
2024-03-14 17:34:12 +0200

    add task 1-3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,25 @@
+class Key {
+  signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  door: boolean;
+  key: Key{};
+  tenants: Person[];
+  comeIn(person: Person);
+}
+class MyHouse extends House {
+  openDoor();
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +28,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
